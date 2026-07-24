5577e40a0d2666eeda0a838c71477bee04ff855a
Kavya Patel <kavyapatel30107@gmail.com>
2026-07-24 10:21:04 +0530

    Add files via upload

---
+++ Biodata.cpp
@@ -0,0 +1,51 @@
+ #include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "********** BIODATA **********" << endl;
+    cout << "\n**Personal Details**" << endl;
+    cout << "Name           : Kavya Patel" << endl;
+    cout << "Father's Name  : Haresh Patel" << endl;
+    cout << "Mother's Name  : Sonal Patel" << endl;
+    cout << "Date of Birth  : 30/10/2007" << endl;
+    cout << "Gender         : Female" << endl;
+    cout << "Age            : 19 Years" << endl;
+    cout << "Nationality    : Indian" << endl;
+    cout << "Religion       : Hindu" << endl;
+    cout << "Blood Group    : A+" << endl;
+    cout << "Height         : 170 cm" << endl;
+    cout << "Weight         : 45 kg" << endl;
+    cout << "Languages Known: Gujarati,English,Hindi" << endl;
+    cout << "Hobbies        : Coding,Travelling,Listening Music" << endl;
+    cout << "Strengths      : Hardworking,Quick Learner" << endl;
+
+    cout << "\n**Contact Details**"<< endl;
+    cout << "Address        : Anand, Gujarat" << endl;
+    cout << "Mobile Number  : 7622885251" << endl;
+    cout << "Email          : kavyapatel@gmail.com" << endl;
+
+    cout << "\n**Educational Details**" << endl;
+    cout << "10th Board     : GSEB" << endl;
+    cout << "School Name    : I.B.Patel English School" << endl;
+    cout << "Passing Year   : 2023" << endl;
+    cout << "Percentage     : 88%" << endl;
+    cout << "\n12th Board   : GSEB" << endl;
+    cout << "School Name    : R.P.T.P Higher Secondary Education Complex(Science Stream)" << endl;
+    cout << "Stream         : Science" << endl;
+    cout << "Passing Year   : 2025" << endl;
+    cout << "Percentage     : 85%" << endl;
+
+    cout << "\nCollege Name : BVM Engineering College, V.V.Nagar" << endl;
+    cout << "Course         : B.Tech Information Technology Engineering" << endl;
+    cout << "Semester       : 3rd Semester" << endl;
+
+    cout << "\n**Technical Skills**" << endl;
+    cout << "Programming      : C,C++" << endl;
+    cout << "Web Technologies : HTML,CSS,Javascript "<< endl;
+    cout << "Operating System : Windows,Linux" << endl;
+
+    cout << "\n**Career Objective**" << endl;
+    cout << "To become a successful software engineer." << endl;
+return 0;
+}
