4f7363598760892e92258284a5e05d38dbd4554b
Kavya Patel <kavyapatel30107@gmail.com>
2026-07-26 11:06:04 +0530

    Add files via upload

---
+++ Display value.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Name      : Kavya Patel" << endl;
+    cout << "Age       : 19" << endl;
+    cout << "Course    : B.Tech IT Engineering" << endl;
+    cout << "College   : BVM Engineering College" << endl;
+    cout << "City      : Anand" << endl;
+
+    return 0;
+}
