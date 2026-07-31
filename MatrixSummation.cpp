f611eba7be11b279051b0e52082402a110381fa8
Kavya Patel <kavyapatel30107@gmail.com>
2026-07-31 10:18:04 +0530

    Add files via upload

---
+++ MatrixSummation.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[10][10], r, c;
+
+    cout << "Enter number of rows and columns: ";
+    cin >> r >> c;
+
+    cout << "Enter matrix elements:\n";
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    cout << "\nRow-wise Sum:\n";
+    for (int i = 0; i < r; i++) {
+        int sum = 0;
+        for (int j = 0; j < c; j++) {
+            sum += a[i][j];
+        }
+        cout << "Sum of Row " << i + 1 << " = " << sum << endl;
+    }
+
+    return 0;
+}
